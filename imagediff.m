dd437ba85c36488078f93985e4ff804232eda794
Anton Dogadov <toxadog@gmail.com>
2016-05-27 11:04:38 +0200

    ajoute imagediff.m

---
+++ imagediff.m
@@ -0,0 +1,20 @@
+function Idiff = imagediff(I,ang,step)
+%Diff?renciation
+ly=size(I,1);
+lx=size(I,2);
+dy=round(step*sin(ang));
+dx=round(step*cos(ang));
+if (dx>=0)&&(dy>=0)
+    Idiff=I(1:ly-dy,1:lx-dx)-I(dy+1:ly,dx+1:lx);
+else if (dx<=0)&&(dy>=0)
+        Idiff=I(1:ly-dy,1-dx:lx)-I(dy+1:ly,1:lx+dx);
+    else if (dx>=0)&&(dy<=0)
+            Idiff=I(1-dy:ly,1:lx-dx)-I(1:ly+dy,dx+1:lx);
+            else if (dx<=0)&&(dy<=0)
+                    Idiff=I(1-dy:ly,1-dx:lx)-I(1:ly+dy,1:lx+dx);
+                end
+        end
+    end
+end
+end
+
